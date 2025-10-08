6f49259a24297246b9a1ef4f8276600bcdb56104
SalimTNKuho <salimalfaruq_2327240061@mhs.mdp.ac.id>
2025-10-08 14:56:56 +0700

    Fitur rumus Luas dan Keliling

--- bin/dart_persegi_panjang.dart
+++ bin/dart_persegi_panjang.dart
@@ -1,7 +1,16 @@
 class PersegiPanjang {
-
   int panjang;
   int lebar;
 
   PersegiPanjang(this.panjang, this.lebar);
+
+  // fungsi luas persegi panjang
+  int luasPersegiPanjang() {
+    return panjang * lebar;
+  }
+
+  // fungsi keliling persegi panjang
+  int kelilingPersegiPanjang() {
+    return 2 * (panjang + lebar);
+  }
 }
